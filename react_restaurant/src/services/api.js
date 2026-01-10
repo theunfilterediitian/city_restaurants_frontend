9dd5b89a2492c5afd89078f17e874669a4f9adad
Arpit Anand <thearpit1@gmail.com>
2026-01-10 07:15:06 +0530

    baseurl fix

--- react_restaurant/src/services/api.js
+++ react_restaurant/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
   // baseURL: 'http://3.110.185.186:8000',
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: '/api',
   // headers: { 'Content-Type': 'application/json' }
 });
 
